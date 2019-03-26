e84a3f5f74cbee7522a7a1daf38fb003c9d64daa
MaheerFarooq <mafarooq1797@gmail.com>
2019-03-26 20:11:00 +0500

    added Test cases and Text files

--- test/poet/GraphPoetTest.java
+++ test/poet/GraphPoetTest.java
@@ -5,21 +5,84 @@ package poet;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
+
+import java.io.File;
 
 /**
  * Tests for GraphPoet.
  */
 public class GraphPoetTest {
     
-    // Testing strategy
-    //   TODO
-    
+    /** Testing strategy
+    -Test assertions enabled
+	-Test when corpus doesn't exist
+	-Test when corpus is empty
+	-Test when input is empty
+	-Test that spaces between words are one space long and words contain letters only
+	-Test more than one space, words contain letters and numbers
+    */
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
     
     // TODO tests
+ // corpus doesn't exist
+    @Test(expected=IOException.class)
+    public void testNonexistentCorpus() throws IOException {
+        File givenCorpus = new File("test/poet/doesntExist.txt");
+        GraphPoet graph = new GraphPoet(givenCorpus);
+    }
     
+    // corpus is empty
+    @Test
+    public void testPoemEmptyCorpus() throws IOException {
+        File givenCorpus = new File("test/poet/empty.txt");
+        GraphPoet graph = new GraphPoet(givenCorpus);
+        String input = "test the empty one";
+        String outputPoem = graph.poem(input);
+        assertEquals(input, outputPoem);
+    }
+    
+    // input is empty
+    @Test
+    public void testPoemEmptyInput() throws IOException {
+        File givenCorpus = new File("test/poet/mugar-omni-theater.txt");
+        GraphPoet graph = new GraphPoet(givenCorpus);
+        String input = "";
+        String outputPoem = graph.poem(input);
+        assertEquals(input, outputPoem);
+    }
+    
+    // all spaces between words are one space long and words contain letters only
+    @Test
+    public void testPoem() throws IOException {
+        File givenCorpus = new File("test/poet/mugar-omni-theater.txt");
+        GraphPoet graph = new GraphPoet(givenCorpus);
+        //System.out.println(graph.toString());
+        String input = "to new worlds.";
+        String outputPoem = graph.poem(input);
+        String correctPoem = "to explore new worlds.";
+        System.out.print(outputPoem);
+        System.out.print(correctPoem);
+        assertEquals(correctPoem, outputPoem);
+    }
+    
+    // more than one space, words contain letters and numbers
+    @Test
+    public void testPoemLettersOrNumbers() throws IOException {
+        File givenCorpus = new File("test/poet/mugar-omni-theater.txt");
+        GraphPoet graph = new GraphPoet(givenCorpus);
+        //System.out.println(graph.toString());
+        String input = "to seek new 1 civiisations";
+        String outputPoem = graph.poem(input);
+        String correctPoem = "to seek out new life 1 new civilizations";
+        System.out.print(outputPoem);
+        System.out.print(correctPoem);
+        assertEquals(correctPoem, outputPoem);
+    }
 }
+
